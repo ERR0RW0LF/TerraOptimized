40b601644851d9ead5997600183757cb7702bb2e
ERR0RW0LF <finn.woma@gmail.com>
2025-02-02 22:43:02 +0100

    Add basic text editor project and enhance args_test with user input prompt

--- Learning-Projects/args_test/src/main.rs
+++ Learning-Projects/args_test/src/main.rs
@@ -1,8 +1,13 @@
 use std::env;
+use std::io::Read;
 
 fn main() {
     let args = env::args();
     for arg in args {
         println!("{}", arg);
     }
+
+    // wait for user input before closing the window
+    println!("Press any key to exit...");
+    let _ = std::io::stdin().read(&mut [0u8]).unwrap();
 }
